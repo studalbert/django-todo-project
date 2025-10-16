2f2c461e5898476dec09c4c4cb6f14ba29e71483
user <albertik_1098@mail.ru>
2025-10-16 17:57:15 +0500

    Django CBV. Знакомство с представлениями, Class-Based Views.

---
+++ todoproject/todo_list/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+from todo_list.views import index_view, ToDoListIndexView, ToDoListView, ToDoListDoneView, ToDoDetailView
+
+app_name = "todo_list"
+
+urlpatterns = [
+    # path('', index_view, name="index"),
+    # path('', ToDoListIndexView.as_view(), name="index"),
+    path('', ToDoListIndexView.as_view(), name="index"),
+    path('<int:pk>/', ToDoDetailView.as_view(), name="detail"),
+    path('list/', ToDoListView.as_view(), name="list"),
+    path('done/', ToDoListDoneView.as_view(), name="done"),
+]
--- todoproject/todo_list/views.py
+++ todoproject/todo_list/views.py
@@ -1,3 +1,35 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
 
-# Create your views here.
+from todo_list.models import ToDoItem
+
+
+def index_view(request: HttpRequest) -> HttpResponse:
+    todo_items = ToDoItem.objects.all()
+    context = {"todo_items": todo_items[:3]}
+    return render(request, template_name="todo_list/index.html", context=context)
+
+
+
+# class ToDoListIndexView(TemplateView):
+#     template_name = "todo_list/index.html"
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["todo_items"] = ToDoItem.objects.all()
+#         return context
+
+class ToDoListView(ListView):
+    model = ToDoItem
+
+class ToDoListIndexView(ListView):
+    template_name = "todo_list/index.html"
+    queryset = ToDoItem.objects.all()[:3]
+
+class ToDoListDoneView(ListView):
+    queryset = ToDoItem.objects.filter(done=True).all()
+
+class ToDoDetailView(DetailView):
+    model = ToDoItem
+
