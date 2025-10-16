6b8d4d848dd5378ad49335841408cf2ca1bc67dc
user <albertik_1098@mail.ru>
2025-10-16 15:52:55 +0500

    add app todo_list and change settings.py

---
+++ todoproject/todo_list/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class ToDoItem(models.Model):
+    class Meta:
+        verbose_name = "ToDo Item"
+    title = models.CharField(max_length=250)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
